bb520877ae8c33796830107794343e3b668dceca
Asaf Kali <akali93@gmail.com>
2022-05-18 23:46:14 +0300

    Add game runner to module init.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ ALL_DEPS = WEB_DEPS
 
 setup(
     name="codenames",
-    version="1.0.1",
+    version="1.0.2",
     description="Codenames board game logic implementation in python.",
     author="Asaf Kali",
     author_email="asaf.kali@mail.huji.ac.il",
