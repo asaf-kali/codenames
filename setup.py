3511f30456536f62bb39cfc2f6cbcffc7afb0fae
Asaf Kali <asaf.kali@mail.huji.ac.il>
2023-04-07 18:36:49 +0300

    String method for enums.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ ALL_DEPS = BASE_DEPS + WEB_DEPS
 
 setup(
     name="codenames",
-    version="1.2.2",
+    version="1.2.3",
     description="Codenames board game logic implementation in python.",
     author="Asaf Kali",
     author_email="asaf.kali@mail.huji.ac.il",
