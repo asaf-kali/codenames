09eb0047c1dea68ce07ad28d3ae09d882c975920
Asaf Kali <asaf.kali@mail.huji.ac.il>
2024-01-06 13:45:22 +0200

    🔨 Tests: remove duplicate file.

--- tests/board_test.py
+++ tests/board_test.py
@@ -1,6 +1,8 @@
 import pytest
 
+from codenames.boards.builder import SupportedLanguage, generate_board
 from codenames.game.board import Board, two_integer_factors
+from codenames.game.color import TeamColor
 from codenames.game.exceptions import CardNotFoundError
 from tests.utils import constants
 
@@ -67,3 +69,38 @@ def test_str_is_printable_string(board_10: Board, board_25: Board):
 
     print()
     print(constants.hebrew_board().printable_string)
+
+
+def test_basic_english_board_build():
+    board = generate_board(language="english")
+    _validate_standard_board(board)
+
+
+def test_basic_hebrew_board_build():
+    board = generate_board(language=SupportedLanguage.HEBREW)
+    _validate_standard_board(board)
+
+
+def test_build_board_with_params():
+    board = generate_board(
+        language=SupportedLanguage.ENGLISH,
+        board_size=17,
+        black_amount=2,
+        seed=42,
+        first_team=TeamColor.RED,
+    )
+    assert len(board.cards) == 17
+    assert len(board.revealed_cards) == 0
+    assert len(board.red_cards) == 6
+    assert len(board.blue_cards) == 5
+    assert len(board.gray_cards) == 4
+    assert len(board.black_cards) == 2
+
+
+def _validate_standard_board(board: Board):
+    assert len(board.cards) == 25
+    assert len(board.revealed_cards) == 0
+    assert len(board.red_cards) >= 8
+    assert len(board.blue_cards) >= 8
+    assert len(board.gray_cards) == 7
+    assert len(board.black_cards) == 1
